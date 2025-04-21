4d10769a4b4ec43929777f03d58b1ab9fce684f1
taararara <43010424+taararara@users.noreply.github.com>
2025-04-21 19:16:44 +0200

    code style

---
+++ lesson_3.py
@@ -0,0 +1,13 @@
+some_string = "How are u today ? "
+upper = some_string.upper()
+low = some_string.lower()
+title = some_string.title()
+capitalaze = some_string.capitalize()
+
+clear_strint = some_string.strip("u")
+print(clear_strint)
+
+change_inner_text = some_string.replace("today", "todayyyy")
+
+
+pass
