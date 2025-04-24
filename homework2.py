16b2c1bf4c082914def78e4e9dce9daa3495c930
taararara <43010424+taararara@users.noreply.github.com>
2025-04-24 22:46:08 +0200

    add hom1 and hom2

---
+++ homework2.py
@@ -0,0 +1,10 @@
+your_numbers = int(input("Введи 4-х значне число: "))
+num1 = your_numbers // 1000
+num2 = your_numbers // 100 % 10
+num3 = your_numbers // 10 % 10
+num4 = your_numbers % 10
+
+print(num1)
+print(num2)
+print(num3)
+print(num4)
---
+++ homework3.py
@@ -0,0 +1,3 @@
+numbers = input("5-ти значне число: ")
+reversed_numbers = numbers[::-1]
+print(reversed_numbers)
