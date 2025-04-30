08beb27d06b9beb8adaa1392a3f2f32f3d4930c1
taararara <43010424+taararara@users.noreply.github.com>
2025-05-01 01:41:42 +0200

    homewoerk 3.3

---
+++ homework3_3.py
@@ -0,0 +1,9 @@
+my_list = [1, 2, 3, 4, 5, 6, 7]
+my_list2 = []
+my_list23 = []
+mid_list = (len(my_list) + 1) // 2
+first_half = my_list[:mid_list]
+second_half = my_list[mid_list:]
+two_halfs_list = [first_half, second_half]
+
+pass
